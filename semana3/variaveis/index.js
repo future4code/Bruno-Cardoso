9559115f2f103289f0e4f03be94617c14ade6e07
brunoCards <bruno.cards27@gmail.com>
2020-07-27 15:21:16 -0300

    Exercicios finalizados

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,96 @@
+// Interpretação de código
+
+/* 
+Ex 1.
+
+a = 10
+b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+Aparecerá 10 no primeiro console.log depois aparecerá 10 e 5 no segundo concole.log
+ */
+
+/*
+Ex 2.
+
+a = 10
+b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+Aparecerá 10 10 10.
+*/
+
+// Escrita de código
+
+// Ex 1.
+
+// let nome
+// let idade
+
+// console.log(typeof(nome, idade))
+
+// // Apareceu undefined, pois não foi atribúido nenhum valor ás variáveis
+
+// nome = prompt("qual o seu nome?")
+// idade = prompt("Qual a sua idade?")
+
+// console.log(typeof(nome, idade))
+
+// // Apareceu undefined e string pq eu não defini qual era o tipo de variável e a linguagem de alguma forma reconheceu o número como uma string.
+
+// console.log("Olá", nome,"você tem", idade, "anos")
+
+
+// // Ex 2.
+
+// let nomeCompleto = prompt("Qual o seu nome completo?")
+// console.log("O meu nome completo é : ", nomeCompleto)
+
+// let estadoCivil = prompt("Qual o seu estado civil?")
+// console.log("Eu sou : ", estadoCivil)
+
+// let profissao = prompt("Qual a sua profissão?")
+// console.log("Eu sou : ", profissao)
+
+// let numeroDeFilhos = prompt("Você tem quantos filhos?")
+// console.log("Eu tenho", numeroDeFilhos, "Filhos")
+
+// let enderecoCompleto = prompt("Qual o sue endereço completo?")
+// console.log(enderecoCompleto)
+
+// // Ex 3.
+
+// let comidasFavoritasDoBruno = ["macarrão com salcicha", "lasanha", "feijão tropeiro", "churrasco", "Purê de mandioquinha da dona Cida"]
+// console.log(comidasFavoritasDoBruno)
+
+// console.log("Essas são as minhas comidas preferidas: ", comidasFavoritasDoBruno)
+
+// let umaComidaPreferida = prompt("Qual a sua comida preferida?")
+// comidasFavoritasDoBruno[1] = umaComidaPreferida
+
+// console.log(comidasFavoritasDoBruno)
+
+// Ex 4.
+
+let perguntas = ["Você acordou cedo hoje?", "Você gosta de animais?", "Você tem cachorros?"]
+let respostas = [true, true, true]
+
+console.log(perguntas[0], respostas[0], perguntas[1], respostas[1], perguntas[2], respostas[2])
+
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[2])
+
+
+
+
+
+
