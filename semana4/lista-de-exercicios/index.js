c0b5f966053dfe88902d8936ab4a1be4815059aa
brunoCards <bruno.cards27@gmail.com>
2020-08-07 13:57:04 -0300

    Ex 1 concluído

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,15 @@
+/*
+    LISTA DE EXERCÍCIOS
+*/
+
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+//Ex 1.
+
+// Inicia a função
+// recebe um parâmetro
+/* A constante cotacao recebe um prompt que terá um cast
+   convertendo a entrada para número */
+// O return da função será uma concatenação juntamente com o parâmetro multiplicado pela variável
+// A constante meuDinheiro recebe a função assumindo um parâmetro ja definido com número 100.
+// o console mostrará o valor da varável meuDinheiro que será 100 * o valor informado pelo usuário.
