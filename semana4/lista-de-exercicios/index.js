93a383dba8e6a0dd85ee85e6e8365aa8f5afc695
brunoCards <bruno.cards27@gmail.com>
2020-08-07 13:57:40 -0300

    Ex 2 concluído

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -13,3 +13,17 @@
 // O return da função será uma concatenação juntamente com o parâmetro multiplicado pela variável
 // A constante meuDinheiro recebe a função assumindo um parâmetro ja definido com número 100.
 // o console mostrará o valor da varável meuDinheiro que será 100 * o valor informado pelo usuário.
+
+
+
+//Ex 2.
+
+// Inicia a função
+// recebe dois parâmetros, tipoDeInvestimento e valor.
+// A variável tipoDeInvestimento que dá inicio a condicional switch caso seja verdade.
+// Na qual em cada case (IF) tem sua condição que são os tipos de investimento, e como default (ELSE) um alert.
+// O return da função será a variavel valorAposInvestimento, para que as condições sejam acessadas fora do bloco da função.
+/* depois são declaradas duas constantes a primeira entra na condição Ações e a segunda entra no default, pois não
+   atinge nenhum dos cases.*/
+// primeiro console mostra o valor de Ações
+// segundo mostra mensagem de TIPO DE INVESTIMENTO INFORMADO INCORRETO.
