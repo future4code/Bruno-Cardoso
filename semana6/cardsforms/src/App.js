c5de1c1029c41bb43ca950f676bd6ff35a948a46
brunoCards <bruno.cards27@gmail.com>
2020-08-17 18:00:38 -0300

    Exercicio concluído, começo da estilização

--- semana6/cardsforms/src/App.js
+++ semana6/cardsforms/src/App.js
@@ -6,15 +6,38 @@ import ThirdStep from "./components/ThirdStep";
 import Congrats from "./components/Congrats";
 
 class App extends React.Component {
+  state = {
+    step: 1,
+  };
+
+  stepRender = () => {
+    switch (this.state.step) {
+      case 1:
+        return <Begin />;
+      case 2:
+        return <SecondStep />;
+      case 3:
+        return <ThirdStep />;
+      case 4:
+        return <Congrats />;
+    }
+  };
+
+  nextStep = () => {
+    const steps = this.setState({ step: this.state.step + 1 });
+  };
+
   render() {
-    return (
-      <div>
-        <Begin />
-        <SecondStep />
-        <ThirdStep />
-        <Congrats />
-      </div>
-    );
+    if (this.state.step <= 3) {
+      return (
+        <div>
+          {this.stepRender()}
+          <button onClick={this.nextStep}>Proxima etapa</button>
+        </div>
+      );
+    } else {
+      return <div>{this.stepRender()}</div>;
+    }
   }
 }
 
