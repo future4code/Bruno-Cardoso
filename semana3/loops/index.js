f46b56a2e247ea05f14e05b71428a002c3c2f578
brunoCards <bruno.cards27@gmail.com>
2020-08-17 13:53:24 -0300

    commit de criação da Branch e do projeto react Js

--- semana3/loops/index.js
+++ semana3/loops/index.js
@@ -9,24 +9,24 @@ for(let i = 0; i < 5; i++) {
 }
 console.log(valor)
 
-* O programa está lendo na primeira linha a variável VALOR com valor 0
-* na segunda linha está usando o laço FOR com a variável I como incremento
-  na CONDIÇÂO que enquanto I for MENOR que 5 INCREMENTE 1 ao valor de I que
-  iniciará o laço com valor 0.
-* na terceira linha ele somará o valor da variável VALOR ao valor de I.
-* na ultima linha imprimirá o valor atual de VALOR.
+// * O programa está lendo na primeira linha a variável VALOR com valor 0
+// * na segunda linha está usando o laço FOR com a variável I como incremento
+//   na CONDIÇÂO que enquanto I for MENOR que 5 INCREMENTE 1 ao valor de I que
+//   iniciará o laço com valor 0.
+// * na terceira linha ele somará o valor da variável VALOR ao valor de I.
+// * na ultima linha imprimirá o valor atual de VALOR.
 
- */
+//  */
 
-/*
-Ex 2.
+// /*
+// Ex 2.
 
-const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
-for (let numero of lista) {
-  if (numero > 18) {
-		console.log(numero)
-	}
-}
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+// }
 
 // a.
         imprimirá o valor de NUMERO SE for MAIOR que 18.
@@ -38,11 +38,11 @@ for (let numero of lista) {
 
 //      Escrita de código
 
-/* 
+/*
 Ex 3.
 */
 
-           const arraOriginal = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const arraOriginal = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // // a.
 //         for(let valor of arraOriginal) {
 //                 console.log(valor)
@@ -63,11 +63,13 @@ Ex 3.
 // }
 
 // d.
-        let indice = 0
-        let arrayDaQuestaoD = [0, 1, 2, 3, 4, 5, 6, 7, 8, 1]
-        for(indice of arrayDaQuestaoD) {
-                if(indice <= arrayDaQuestaoD.length) {
+// let indice = 0
+// let arrayDaQuestaoD = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+// for (indice of arrayDaQuestaoD) {
+//   if (indice <= arrayDaQuestaoD.length) {
+//     console.log(`O elemento do índex ${arrayDaQuestaoD[indice]} é ${indice}`)
+//   }
+// }
 
-                        console.log(`O elemento do índex ${arrayDaQuestaoD[indice]} é ${indice}`)
-                }
-}
+// e.
+let arrayDaQuestaoE = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
