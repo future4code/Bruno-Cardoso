a8ae184a4d7305ab4066c07bfe8c95b5b0ad687c
brunoCards <bruno.cards27@gmail.com>
2020-08-18 16:46:42 -0300

    commit de entrega

--- semana6/lifecycle/boiler-plate/src/App.js
+++ semana6/lifecycle/boiler-plate/src/App.js
@@ -1,85 +1,87 @@
-import React from 'react'
-import styled from 'styled-components'
-import './styles.css'
+import React from "react";
+import styled from "styled-components";
+import "./styles.css";
 
 const TarefaList = styled.ul`
   padding: 0;
   width: 200px;
-`
+`;
 
 const Tarefa = styled.li`
   text-align: left;
-  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
-`
+  text-decoration: ${({ completa }) => (completa ? "line-through" : "none")};
+`;
 
 const InputsContainer = styled.div`
   display: grid;
   grid-auto-flow: column;
   gap: 10px;
-`
+`;
 
 class App extends React.Component {
-    state = {
-      tarefas: [],
-      inputValue: '',
-      filtro: ''
-    }
+  state = {
+    tarefas: [
+      {
+        id: Date.now(),
+        texto: "Treino cloning UI da noite",
+        completa: false,
+      },
 
-  componentDidUpdate() {
-
+      {
+        id: Date.now(),
+        texto: "Assistir Dark",
+        completa: false,
+      },
+    ],
+    inputValue: "",
+    filtro: "pendentes",
   };
 
-  componentDidMount() {
+  componentDidUpdate() {}
 
-  };
+  componentDidMount() {}
 
   onChangeInput = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
 
-  }
+  criaTarefa = () => {};
 
-  criaTarefa = () => {
+  selectTarefa = (id) => {};
 
-  }
-
-  selectTarefa = (id) => {
-
-  }
-
-  onChangeFilter = (event) => {
-
-  }
+  onChangeFilter = (event) => {};
 
   render() {
-    const listaFiltrada = this.state.tarefas.filter(tarefa => {
+    const listaFiltrada = this.state.tarefas.filter((tarefa) => {
       switch (this.state.filtro) {
-        case 'pendentes':
-          return !tarefa.completa
-        case 'completas':
-          return tarefa.completa
+        case "pendentes":
+          return !tarefa.completa;
+        case "completas":
+          return tarefa.completa;
         default:
-          return true
+          return true;
       }
-    })
+    });
 
     return (
       <div className="App">
         <h1>Lista de tarefas</h1>
         <InputsContainer>
-          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+          <input value={this.state.inputValue} onChange={this.onChangeInput} />
           <button onClick={this.criaTarefa}>Adicionar</button>
         </InputsContainer>
-        <br/>
+        <br />
 
         <InputsContainer>
           <label>Filtro</label>
-          <select value={this.state.filter} onChange={this.onChangeFilter}>
+          <select value={this.state.filtro} onChange={this.onChangeFilter}>
             <option value="">Nenhum</option>
             <option value="pendentes">Pendentes</option>
             <option value="completas">Completas</option>
           </select>
         </InputsContainer>
         <TarefaList>
-          {listaFiltrada.map(tarefa => {
+          {listaFiltrada.map((tarefa) => {
             return (
               <Tarefa
                 completa={tarefa.completa}
@@ -87,12 +89,12 @@ class App extends React.Component {
               >
                 {tarefa.texto}
               </Tarefa>
-            )
+            );
           })}
         </TarefaList>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
