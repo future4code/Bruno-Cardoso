f3bc55533d486a4a167585c5b9169bd21ce799b1
brunoCards <bruno.cards27@gmail.com>
2020-10-22 16:10:08 -0300

    primeiro endpoint feito

--- semana13/Express/src/index.ts
+++ semana13/Express/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 
-import { countries } from "./countries";
+import { countries, country } from "./countries";
 
 const app: Express = express();
 app.use(express.json());
@@ -10,12 +10,25 @@ app.use(cors());
 // endpoint 1
 
 app.get("/countries/all", (request: Request, response: Response) => {
-  response.status(200).send(countries);
+  const countryFound = countries.map((country) => ({
+    id: country.id,
+    name: country.name,
+  }));
+  response.status(200).send(countryFound);
+  response.status(400).send("Não foi encontrado nenhum país");
 });
 
 // endpoint 2
 
+app.get("/countries/:id", (request: Request, response: Response) => {
+  const countryFound: country | undefined = countries.find(
+    (country) => country.id === Number(request.params.id)
+  );
 
+  response.status(200).send(countryFound);
+});
+
+// endpoint 3
 
 app.listen(3003, () => {
   console.log("O Servidor está rodando na porta 3003 ... e rodando");
