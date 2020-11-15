20813ac9db843504693a79e5e4771bce898cd2bb
Bruno Cardoso <brunocardoso@pop-os.localdomain>
2020-11-15 18:59:40 -0300

    Organizando pastas

--- semana14/toDoList/src/dbHandlers/handleGetUserById.ts
+++
@@ -1,9 +0,0 @@
-import { connection } from "../";
-
-export default async function handleGetUserById(id: string) {
-  const valueReturn = await connection("TodoListUser")
-    .select("*")
-    .where({ id: id });
-
-  return valueReturn[0];
-}
